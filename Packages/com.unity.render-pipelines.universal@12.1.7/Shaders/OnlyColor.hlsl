e88c387ce58eff0ce407cfed3a1d521876959850
liuyc02 <liuyc02@mail.jj.cn>
2024-01-18 18:45:01 +0800

    package_2

---
+++ Packages/com.unity.render-pipelines.universal@12.1.7/Shaders/OnlyColor.hlsl
@@ -0,0 +1,43 @@
+﻿#ifndef ONLY_COLOR_PASS_INCLUDED
+#define ONLY_COLOR_PASS_INCLUDED
+
+// #include "Packages/com.unity.render-pipelines.universal/Shaders/LitInput.hlsl"
+#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+
+struct Attributes
+{
+    float4 positionOS   : POSITION;
+    float2 texcoord     : TEXCOORD0;
+    UNITY_VERTEX_INPUT_INSTANCE_ID
+};
+
+struct Varyings
+{
+    float2 uv                       : TEXCOORD0;
+    float4 positionCS               : SV_POSITION;
+    UNITY_VERTEX_INPUT_INSTANCE_ID
+    UNITY_VERTEX_OUTPUT_STEREO
+};
+
+Varyings OnlyColorVertex(Attributes input)
+{
+    Varyings output;
+
+    UNITY_SETUP_INSTANCE_ID(input);
+    UNITY_TRANSFER_INSTANCE_ID(input, output);
+    UNITY_INITIALIZE_VERTEX_OUTPUT_STEREO(output);
+    
+    output.positionCS = TransformObjectToHClip(input.positionOS.xyz);
+    output.uv = TRANSFORM_TEX(input.texcoord, _BaseMap);
+    return output;
+}
+
+half4 OnlyColorFragment(Varyings input) : SV_Target
+{
+    UNITY_SETUP_INSTANCE_ID(input);
+    UNITY_SETUP_STEREO_EYE_INDEX_POST_VERTEX(input);
+    
+    return SAMPLE_TEXTURE2D(_BaseMap, sampler_BaseMap, input.uv) * _BaseColor;
+}
+
+#endif
